d61f09ee962a016328668f9677ad830af6e32063
passgiant <passgiant@naver.com>
2024-09-02 13:38:45 +0900

    display_gray

---
+++ display_gray.py
@@ -0,0 +1,11 @@
+import cv2, sys
+from matplotlib import pyplot as plt
+
+fileName = 'data/cat.jpg'
+
+imgGray = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)
+print(imgGray.shape)
+
+plt.axis('off')
+plt.imshow(imgGray, cmap='gray', interpolation='bicubic')
+plt.show()
